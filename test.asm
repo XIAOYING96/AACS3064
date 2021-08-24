49db9c7533b193b1a8526d5bdc22895aa251ebac
LXYing <86389859+XIAOYING96@users.noreply.github.com>
2021-08-24 12:15:25 +0800

    Lecturer Example

--- test.asm
+++ test.asm
@@ -1,46 +1,36 @@
-TITLE TEST
-
 .model small
 .stack
-
-.data 
-	msg1	DB	0dh,0ah,"Enter you password >> $"
-	input	DB	?
-	PWC	DB	5	DUP(0)
+.data
+	myData DB 2,4,7,'H','A',9
 
 .code
 main proc
-	mov ax, @data
+	mov ax,@data
 	mov ds,ax
-
-	;--display message
-	mov ah,09h
-	lea dx,msg1
-	int 21h
-
-	;--get user input
-	mov ax,05h
-	mov dl,03h
-	int 21h
-	mov PWC[dx],al
 	
+	mov si,0
 
-	mov ah,05h
-	mov dl,input
-	int 21h
+	chk:
+		CMP myData[si],"A"
+		JNAE next
+		cmp myData[si],"Z"
+		jbe print
+		ja next
+	next:
+		inc si
+		jmp chk
 
-	mov ah,07h
-	int 21h
+	print:
+		mov ah,02h
+		mov dl,myData[SI]
+		int 21h
 
-	mov ah,05h
-	mov dl,al
-	int 21h
-	
-	mov ah,4ch
-	int 21h
+	exit:
+		mov ah,4ch
+		int 21h
 
 main endp
 end main
 
-
+	
 	
